6a99004ffbb23fd420cfdec267157f49f4494c4e
beywesley1 <beycloudtraining@gmail.com>
2026-01-05 15:24:25 -0600

    Change resume button to force download instead of opening

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5569,7 +5569,7 @@ export default function App() {
               </svg>
               View GitHub
             </a>
-            <a href={CONFIG.resumeUrl} className="btn btn-secondary">
+            <a href={CONFIG.resumeUrl} download="Wesley_Bey_Resume.pdf" className="btn btn-secondary">
               <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
                 <polyline points="14 2 14 8 20 8"/>
